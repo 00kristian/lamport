2b0ce9091f309470cc219bdda190ab1006a4d259
Kristian Berlin <krbj@itu.dk>
2022-01-03 14:30:40 +0100

    tick fungerer men lidt fucked

--- Server/server.go
+++ Server/server.go
@@ -4,14 +4,34 @@ import (
 	"context"
 	"log"
 	"net"
+	"sync"
 
 	"GRPC_demo/chat"
 
 	"google.golang.org/grpc"
 )
 
+type SafeLamport struct {
+	mu sync.Mutex
+	Lamport int64
+	}
+	
+func (c *SafeLamport) Tick() {
+	c.mu.Lock()
+	c.Lamport++
+	defer c.mu.Unlock()
+}
+
+func(c *SafeLamport) CompareClock(otherClockTime int64) {
+	c.mu.Lock()
+	newVal :=  max(c.Lamport, otherClockTime) 
+	c.Lamport = newVal
+	defer c.mu.Unlock()
+}
+
 type Server struct {
 	chat.UnimplementedChatServiceServer
+	SafeLamport SafeLamport
 }
 
 func main() {
@@ -20,7 +40,7 @@ func main() {
 		log.Fatalf("Failed to listen on port 9000: %v", lisErr)
 	}
 
-	s := Server{chat.UnimplementedChatServiceServer{}}
+	s := Server{SafeLamport: SafeLamport{Lamport: 0}}
 
 	grpcServer := grpc.NewServer()
 
@@ -31,10 +51,22 @@ func main() {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", serveErr)
 	}
 
+
 }
 
 
-func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
-	log.Printf("Recieved Message body from client: %s", message.Body)
-	return &Message{Body: "Hello from the server!"}, nil
+func (s *Server) SayHello(ctx context.Context, message *chat.Message) (*chat.Message, error) { 
+	
+	log.Printf("Recieved Message body from client: %s lamport: %d" , message.Body, message.Lamport)
+
+
+	s.SafeLamport.Tick()
+	return &chat.Message{Body: "Hello from the server! wit timestamp", Lamport:s.SafeLamport.Lamport  }, nil
+}
+
+func max(a, b int64) int64 {
+	if a < b {
+		return b
+	}
+	return a
 }
--- client/client.go
+++ client/client.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"log"
+	"sync"
+	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"GRPC_demo/chat"
 )
+
+
+
+var safeL = SafeLamport{}
+
+type SafeLamport struct {
+	mu sync.Mutex
+	Lamport int64
+	}
+
 
 func main() {
 	var conn *grpc.ClientConn
@@ -18,16 +30,44 @@ func main() {
 	defer conn.Close()
 
 	c := chat.NewChatServiceClient(conn)
+	for{
+	safeL.tickmychild(context.Background() ,c)
+	time.Sleep(3*time.Second)
+	}
+
+
+}
+
+
+func (s *SafeLamport) tickmychild (ctx context.Context, client chat.ChatServiceClient){
 
 	message := chat.Message{
 		Body: "Hello people!",
+		Lamport: safeL.Lamport,
 	}
 
-	response, err := c.SayHello(context.Background(), &message)
+	response, err := client.SayHello(context.Background(), &message)
+	
+
 	if err != nil {
 		log.Fatalf("Error when calling SayHello: %s", err)
 	}
 
-	log.Printf("Response from server: %s", response.Body)
+	safeL.CompareTimestamp(response.Lamport)
 
+	log.Printf("Response from server: %s and %d", response.Body, safeL.Lamport)
+}
+
+func (l *SafeLamport) CompareTimestamp(otherClock int64) {
+    l.mu.Lock()
+    newValue := max(l.Lamport, otherClock)
+    l.Lamport = newValue
+    defer l.mu.Unlock()
+}
+
+func max(a, b int64) int64 {
+	if a < b {
+		return b
+	}
+	return a
 }
